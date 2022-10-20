c2f9087d64798d7c86b79052e6dd758cf42d0149
Ahmed Samir <ahmed.samir@espace.com.eg>
2022-10-20 15:46:50 +0200

    Initial Commit

---
+++ GymBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymBuddy
+//
+//  Created by Samir on 10/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
