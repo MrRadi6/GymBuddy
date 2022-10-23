3c206fa44452688e55d7f42e1b3fedf82a8da75d
Ahmed Samir <ahmed.samir@espace.com.eg>
2022-10-23 22:06:54 +0200

    Filter out exercise from exercise variations

--- GymBuddy/Data/Repositories/ExerciseRespository.swift
+++ GymBuddy/Data/Repositories/ExerciseRespository.swift
@@ -26,13 +26,16 @@ struct ExerciseRespository: ExerciseRespositoryProtocol {
     func getExerciseDetails(with id: Int) -> AnyPublisher<ExerciseDetails, NetworkError> {
         remote.getExerciseDetails(with: id)
             .flatMap { exerciseDTO in
-                Publishers.MergeMany(exerciseDTO.variations.map { remote.getExerciseDetails(with: $0) })
-                    .collect()
-                    .map { variationsDTO in
-                        let variations = variationsDTO.map({ Exercise(id: $0.id, name: $0.name )})
-                        let images = exerciseDTO.images.map({ $0.image })
-                        return ExerciseDetails(id: exerciseDTO.id, name: exerciseDTO.name, images: images, variations: variations)
-                    }
+                Publishers.MergeMany(exerciseDTO.variations
+                    .filter({ $0 != exerciseDTO.id })
+                    .map { remote.getExerciseDetails(with: $0)}
+                )
+                .collect()
+                .map { variationsDTO in
+                    let variations = variationsDTO.map({ Exercise(id: $0.id, name: $0.name )})
+                    let images = exerciseDTO.images.map({ $0.image })
+                    return ExerciseDetails(id: exerciseDTO.id, name: exerciseDTO.name, images: images, variations: variations)
+                }
             }.eraseToAnyPublisher()
     }
 }
